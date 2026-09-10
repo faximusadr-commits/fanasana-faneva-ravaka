da3ba3b1a043ff551b9461436c3aa22dc535b2ee
faximusadr-commits <faximus.adr@gmail.com>
2026-09-10 16:49:26 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('notificationclick', (e) => {
+  e.notification.close();
+  e.waitUntil(self.clients.matchAll({ type: 'window' }).then((list) => {
+    for (const c of list) if ('focus' in c) return c.focus();
+    if (self.clients.openWindow) return self.clients.openWindow('./');
+  }));
+});
